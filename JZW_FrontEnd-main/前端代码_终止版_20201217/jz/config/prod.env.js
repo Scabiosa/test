a4b4697e3226d0279cfc168e6690b1ce0888a982
scabiosa <2817001759@qq.com>
2021-03-03 19:43:51 +0800

    delete

--- JZW_FrontEnd-main/前端代码_终止版_20201217/jz/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  API_BASE: '"http://221.224.21.21:8888/ecnu"',
-  Mock: false
-}
--- JZW_FrontEnd-main/前端代码_终止版_20201217/jz/src/i18n/langs/zh_CN_FrontPage.js
+++
@@ -1,7 +0,0 @@
-const zh_CN_FrontPage = {
-    subnetDesc:'旗下网站',
-    title : '鉴真网是关于中日企业的工商、产品、服务、技术和采购信息的大数据咨询平台',
-    more: '更多',
-}
-
-export default zh_CN_FrontPage
